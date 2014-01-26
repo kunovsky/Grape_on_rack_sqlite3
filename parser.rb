89037f0e5e1d2ef3df79e1bbc9a700aafab84370
Tyson Kunovsky <tyson@gottman.com>
2014-01-26 12:15:12 -0600

    deleted the old parser file

--- parser.rb
+++
@@ -1,9 +0,0 @@
-module Parser
-  def self.parser(file)
-    people = []
-    CSV.foreach(file, :headers => true, header_converters: :symbol) do |csv_obj|
-     people << User.new(csv_obj)
-    end
-    people
-  end
-end
