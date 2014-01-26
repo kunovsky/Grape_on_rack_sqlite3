0d570a65b6027bb206673ffc8aa0b7ed37fd9fda
Tyson Kunovsky <tyson@gottman.com>
2014-01-26 11:44:15 -0600

    more rspec configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'capybara/rspec'
+
+require_relative '../config/application.rb'
+
+RSpec.configure do |config|
+
+  config.mock_with :rspec
+  config.expect_with :rspec
+  config.color_enabled = true
+  config.formatter = :documentation
+  config.order = 'random'
+  
+end
+
+
+# Capybara.configure do |config|
+#   config.app = Acme::App.new
+#   config.server_port = 9293
+# end
